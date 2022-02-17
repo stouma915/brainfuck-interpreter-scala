e5d6d19c14aa08e6dfe382483fcb9e23e3a995b8
stouma915 <sasakitouma@yahoo.ne.jp>
2022-02-17 15:51:59 +0900

    Update BrainfuckInterpreter.scala

--- src/main/scala/net/stouma915/brainfuckinterpreter/BrainfuckInterpreter.scala
+++ src/main/scala/net/stouma915/brainfuckinterpreter/BrainfuckInterpreter.scala
@@ -6,110 +6,143 @@ import scala.util.{Success, Try}
 import java.io.File
 import java.nio.file.{Files, Paths}
 
-object BrainfuckInterpreter extends IOApp {
+object BrainfuckInterpreter {
 
-  import cats.effect.unsafe.implicits.global
-
-  override def run(args: List[String]): IO[ExitCode] =
-    IO {
-      if (args.length != 1) {
-        showHelp().unsafeRunSync()
-
-        ExitCode.Error
-      } else {
-        val sourceFileName = args.head
-
-        if (sourceFileName.startsWith("-")) {
-          sourceFileName match {
-            case "--help" =>
-              showHelp().unsafeRunSync()
-
-              ExitCode.Success
-            case "-h" =>
-              showHelp().unsafeRunSync()
-
-              ExitCode.Success
-            case _ =>
-              val program = for {
-                _ <- IO {
-                  println(
-                    s"Found argument '$sourceFileName' which wasn't expected, or isn't valid in this context."
-                  )
-                  println("")
-                }
-                _ <- showUsage()
-                _ <- IO {
-                  println("")
-                  println("For more information try --help")
-                }
-              } yield ()
-              program.unsafeRunSync()
-
-              ExitCode.Error
-          }
-        } else {
-          Try(Files.readString(Paths.get(sourceFileName)))
-            .map { source =>
-              IO {
-                val sourceWithoutReturns = source.replaceAll("\n", "")
-
-                implicit val _memory: Memory = Memory.Empty
-
-                val result =
-                  Interpreter
-                    .evaluate(sourceWithoutReturns)
-                    .unsafeRunSync()
-
-                result match {
-                  case Right(x) =>
-                    println(x._1)
-
-                    ExitCode.Success
-                  case Left(err) =>
-                    println("Execution failed.")
-                    println(err)
-
-                    ExitCode.Error
-                }
-              }
-            }
-            .recoverWith { case e: Exception =>
-              Success {
-                IO {
-                  println(s"Unable to read source file: $e")
-
-                  ExitCode.Error
-                }
-              }
-            }
-            .get
-            .unsafeRunSync()
-        }
+  private[brainfuckinterpreter] def showHelp(): IO[Unit] =
+    for {
+      _ <- IO {
+        println("Brainf**k Interpreter 1.0.0")
+        println("")
       }
-    }
+      _ <- showUsage()
+      _ <- IO {
+        println("""
+                  |Flags:
+                  |    -h, --help    Prints help information.
+                  |    
+                  |Args:
+                  |    <SOURCE> Brainfuck source file.
+                  |""".stripMargin)
+      }
+    } yield ()
 
-  private def showHelp(): IO[Unit] = IO {
-    println("Brainf**k Interpreter 1.0.0")
-    println("")
-    showUsage().unsafeRunSync()
-    println("""
-        |Flags:
-        |    -h, --help    Prints help information.
-        |    
-        |Args:
-        |    <SOURCE> Brainfuck source file.
-        |""".stripMargin)
-  }
+  private[brainfuckinterpreter] def showUsage(): IO[Unit] =
+    for {
+      _ <- IO(println("Usage:"))
+      progName <- getProgramName
+      _ <- IO(println(s"    $progName [FLAGS] [SOURCE]"))
+    } yield ()
 
-  private def showUsage(): IO[Unit] = IO {
-    println("Usage:")
-    println(s"    ${getProgramName.unsafeRunSync()} [FLAGS] [SOURCE]")
-  }
-
-  private def getProgramName: IO[String] = IO {
+  private[brainfuckinterpreter] def getProgramName: IO[String] = IO {
     new File(
       BrainfuckInterpreter.getClass.getProtectionDomain.getCodeSource.getLocation.getPath
     ).getName
   }
 
+  private[brainfuckinterpreter] def exit(exitCode: ExitCode): IO[Unit] = IO {
+    sys.exit(exitCode.code)
+  }
+
+}
+
+@main
+def interpreter(args: String*): Unit = {
+
+  import cats.effect.unsafe.implicits.global
+  import BrainfuckInterpreter.*
+
+  val program = IO {
+    if (args.length != 1) {
+      val program = for {
+        _ <- showHelp()
+        _ <- exit(ExitCode.Error)
+      } yield ()
+
+      program.unsafeRunSync()
+    } else {
+      val sourceFileName = args.head
+
+      if (sourceFileName.startsWith("-")) {
+        sourceFileName match {
+          case "--help" =>
+            val program = for {
+              _ <- showHelp()
+              _ <- exit(ExitCode.Success)
+            } yield ()
+
+            program.unsafeRunSync()
+          case "-h" =>
+            val program = for {
+              _ <- showHelp()
+              _ <- exit(ExitCode.Success)
+            } yield ()
+
+            program.unsafeRunSync()
+          case _ =>
+            val program = for {
+              _ <- IO {
+                println(
+                  s"Found argument '$sourceFileName' which wasn't expected, or isn't valid in this context."
+                )
+                println("")
+              }
+              _ <- showUsage()
+              _ <- IO {
+                println("")
+                println("For more information try --help")
+              }
+              _ <- exit(ExitCode.Error)
+            } yield ()
+
+            program.unsafeRunSync()
+        }
+      } else {
+        Try(Files.readString(Paths.get(sourceFileName)))
+          .map { source =>
+            IO {
+              val sourceWithoutReturns = source.replaceAll("\n", "")
+
+              implicit val _memory: Memory = Memory.Empty
+
+              val result =
+                Interpreter
+                  .evaluate(sourceWithoutReturns)
+                  .unsafeRunSync()
+
+              result match {
+                case Right(x) =>
+                  val program = for {
+                    _ <- IO(println(x._1))
+                    _ <- exit(ExitCode.Success)
+                  } yield ()
+
+                  program.unsafeRunSync()
+                case Left(err) =>
+                  val program = for {
+                    _ <- IO {
+                      println("Execution failed.")
+                      println(err)
+                    }
+                    _ <- exit(ExitCode.Error)
+                  } yield ()
+
+                  program.unsafeRunSync()
+              }
+            }
+          }
+          .recoverWith { case e: Exception =>
+            Success {
+              for {
+                _ <- IO(println(s"Unable to read source file: $e"))
+                _ <- exit(ExitCode.Error)
+              } yield ()
+            }
+          }
+          .get
+          .unsafeRunSync()
+      }
+    }
+  }
+
+  program.unsafeRunSync()
 }
